98f16a6fc90f83f21bc798bae3a8f3c592c7e910
gus4214 <98795239+gus4214@users.noreply.github.com>
2022-10-08 21:03:14 +0900

    짝수와홀수

---
+++ 짝수와홀수.js
@@ -0,0 +1,16 @@
+function solution(num) {
+  let answer;
+  if (num % 2 === 0) {
+    answer = "Even";
+  } else {
+    answer = "Odd";
+  }
+  return answer;
+}
+
+console.log(solution(5));
+
+// 다른 풀이
+function evenOrOdd(num) {
+  return num % 2 ? "Odd" : "Even";
+}
