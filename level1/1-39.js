1bce73043fac808d967ed365ac536888efc9a01a
gus4214 <98795239+gus4214@users.noreply.github.com>
2022-11-18 16:24:05 +0900

    Init

---
+++ level1/1-39.js
@@ -0,0 +1,25 @@
+function solution(s) {
+  const arr = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+
+  let answer = s;
+
+  for (let i = 0; i < arr.length; i++) {
+    let result = answer.split(arr[i]);
+    answer = result.join(i);
+    console.log(answer);
+  }
+  return parseInt(answer);
+}
+
+console.log(solution("2twotwo45four"));
